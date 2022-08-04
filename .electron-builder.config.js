9962ae0fc42f026f898eb7f6b04f26e818f22bf5
yeojongki <yeojongki@foxmail.com>
2022-08-05 00:26:32 +0800

    refactor(build): modify nsis config

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -1,7 +1,11 @@
-const pkg = require('./package.json')
+const pkg = require("./package.json");
 if (process.env.VITE_APP_VERSION === undefined) {
-  const now = new Date;
-  process.env.VITE_APP_VERSION = pkg.version || `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
+  const now = new Date();
+  process.env.VITE_APP_VERSION =
+    pkg.version ||
+    `${now.getUTCFullYear() - 2000}.${
+      now.getUTCMonth() + 1
+    }.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
 }
 
 /**
@@ -11,16 +15,19 @@ if (process.env.VITE_APP_VERSION === undefined) {
 const config = {
   productName: "打工人的Excel",
   directories: {
-    output: 'dist',
-    buildResources: 'buildResources',
+    output: "dist",
+    buildResources: "buildResources",
   },
-  files: [
-    'packages/**/dist/**',
-  ],
+  files: ["packages/**/dist/**"],
   extraMetadata: {
     version: process.env.VITE_APP_VERSION,
   },
   npmRebuild: false,
+  nsis: {
+    oneClick: false,
+    perMachine: true,
+    allowToChangeInstallationDirectory: true,
+  },
 };
 
 module.exports = config;
